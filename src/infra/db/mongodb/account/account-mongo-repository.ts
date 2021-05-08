76b11d9b9a2e4612c2348fa888fe68c43085d5da
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-05-08 15:37:38 -0300

    feat: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,5 +1,6 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository'
+import { LoadAccountByTokenRepository } from '../../../../data/protocols/db/account/load-account-by-token-repository'
 import { UpdateAcessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
@@ -8,7 +9,8 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepoistory implements
 AddAccountRepository,
 LoadAccountByEmailRepository,
-UpdateAcessTokenRepository {
+UpdateAcessTokenRepository,
+LoadAccountByTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -32,5 +34,11 @@ UpdateAcessTokenRepository {
         }
       }
     )
+  }
+
+  async loadByToken (token: string, role?: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ accessToken: token, role })
+    return account && MongoHelper.map(account)
   }
 }
