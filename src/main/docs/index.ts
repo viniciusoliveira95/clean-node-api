ab1c088f8d41f33b26535e03cafab4f2cb1f0268
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-05-19 13:34:41 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
-import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
+import { badRequest, serverError, unauthorized, notFound } from './components'
 
 export default {
   openapi: '3.0.0',
@@ -24,6 +24,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
