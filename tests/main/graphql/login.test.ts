a464337438da7c9fef36cb53d9385d7a664e3086
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-05-30 12:47:38 -0300

    test: ensure Login Query returns UnauthorizedError on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -51,5 +51,17 @@ describe('Login GraphQL', () => {
       expect(res.data.login.accessToken).toBeTruthy()
       expect(res.data.login.name).toBe('Vinicius')
     })
+
+    test('Should return UnauthorizedError on invalid credentials', async () => {
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(loginQuery, {
+        variables: {
+          email: 'vini@mail.com',
+          password: '123'
+        }
+      })
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Unauthorized')
+    })
   })
 })
