50c19f6f0c4d3777af5a92edf7eea348c076ad0b
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-05-30 14:05:20 -0300

    test: ensure SaveSurveyResult Query returns SurveyResult on success

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -2,8 +2,11 @@ import { IController } from '@/presentation/protocols'
 
 import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-errors'
 
-export const adaptResolver = async (controller: IController, args?: any): Promise<any> => {
-  const request = { ...(args || {}) }
+export const adaptResolver = async (controller: IController, args?: any, context?: any): Promise<any> => {
+  const request = {
+    ...(args || {}),
+    accountId: context?.req?.accountId
+  }
   const httpResponse = await controller.handle(request)
   switch (httpResponse.statusCode) {
     case 200: return httpResponse.body
--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -114,4 +114,54 @@ describe('SurveyResult GraphQL', () => {
       expect(res.errors[0].message).toBe('Access denied')
     })
   })
+
+  describe('SaveSurveyResult Mutation', () => {
+    const saveSurveyResultMutation = gql`
+      mutation saveSurveyResult ($surveyId: String!, $answer: String!) {
+        saveSurveyResult (surveyId: $surveyId, answer: $answer) {
+          question
+          answers {
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+          date
+        }
+      }
+    `
+
+    test('Should return SurveyResult', async () => {
+      const accessToken = await makeAccessToken()
+      const surveyRes = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const { mutate } = createTestClient({
+        apolloServer,
+        extendMockRequest: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      })
+      const res: any = await mutate(saveSurveyResultMutation, {
+        variables: {
+          surveyId: surveyRes.ops[0]._id.toString(),
+          answer: 'Answer 1'
+        }
+      })
+      expect(res.data.saveSurveyResult.question).toBe('Question')
+      expect(res.data.saveSurveyResult.answers[0].answer).toBe('Answer 1')
+      expect(res.data.saveSurveyResult.answers[0].count).toBe(1)
+      expect(res.data.saveSurveyResult.answers[0].percent).toBe(100)
+      expect(res.data.saveSurveyResult.answers[0].isCurrentAccountAnswer).toBe(true)
+    })
+  })
 })
